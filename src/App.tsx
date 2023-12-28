c461a6f7053a38c10d6c5572343e1398560c7891
Ramon Saboya <ramonsaboya@fb.com>
2023-12-28 05:12:03 +0200

    Add button to fetch game server URL

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [response, setResponse] = useState('');
+
+  const handleClick = async () => {
+    const res = await fetch(process.env.REACT_APP_REQUEST_GAME_SERVER_LAMBDA_URL || '');
+    const data = await res.text();
+    setResponse(data);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={handleClick}>
+          <img src={logo} className="App-logo" alt="logo" />
+        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {response || 'XXXX'}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
