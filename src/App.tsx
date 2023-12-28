61005206cffbdbaf818a247e6184002a2473d5ba
Ramon Saboya <ramonsaboya@fb.com>
2023-12-28 05:59:25 +0200

    Add loading state

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,18 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   const [response, setResponse] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleClick = async () => {
+    if (isLoading) {
+      return;
+    }
+
+    setIsLoading(true);
+
     fetch(process.env.REACT_APP_REQUEST_GAME_SERVER_LAMBDA_URL || '', {
       method: 'POST',
       headers: {
@@ -16,7 +23,8 @@ function App() {
     })
       .then(res => res.text())
       .then(data => setResponse(data))
-      .catch(error => setResponse('Error: ' + error.message));
+      .catch(error => setResponse('Error: ' + error.message))
+      .finally(() => setIsLoading(false));
   };
 
   return (
@@ -27,15 +35,17 @@ function App() {
           <label htmlFor="password">Password:</label>
           <input type="text" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         </div>
-        <button onClick={handleClick}>
+        <button onClick={handleClick} disabled={isLoading}>
           Submit
         </button>
-        <p>
-          {response || '<server address>'}
-        </p>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <p>
+            {response || '<server address>'}
+          </p>
+        )}
       </header>
     </div>
   );
 }
-
-export default App;
