5b04519e43f2a89b63a6cbff0d3d9375f6a1ac60
Ramon Saboya <ramonsaboya@fb.com>
2023-12-29 20:07:42 +0200

    Add player join endpoint basic

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,34 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 4000;
 
-// Start the server
+const players: { name: string }[] = [];
+const maxPlayers = 4;
+
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://ramonsaboya.github.io']
+}));
+app.use(express.json());
+
+app.post('/join', (req, res) => {
+    const playerName: string = req.body.playerName;
+
+    const playerExists = players.some(player => player.name === playerName);
+    if (playerExists) {
+        return res.status(400).json({ error: 'Player name already exists' });
+    }
+
+    if (players.length >= maxPlayers) {
+        return res.status(400).json({ error: 'Room is full' });
+    }
+
+    players.push({ name: playerName });
+
+    return res.status(200).json({ message: 'Player joined successfully' });
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
--- webapp/src/game/GamePage.tsx
+++ webapp/src/game/GamePage.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import logo from './../logo.svg';
+import React, {useState} from 'react';
 import './../App.css';
 import {useLoaderData} from '../useLoaderData';
 import {LoaderFunctionArgs, Params} from 'react-router-dom';
 import {isValidIpAddress} from '../utils';
+import {getErrorMessage} from '../error_handling';
 
 type GameLoaderData = {
   serverAddress: string | undefined;
@@ -22,12 +22,48 @@ export async function loader({
 export default function GamePage() {
   const {serverAddress} = useLoaderData<GameLoaderData>();
 
+  const [playerName, setPlayerName] = useState('');
+  const [apiResponse, setApiResponse] = useState('');
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      console.log(playerName);
+      const response = await fetch(`http://${serverAddress}:4000/join`, {
+        method: 'POST',
+        body: JSON.stringify({playerName}),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (response.ok) {
+        setApiResponse('Success');
+      } else {
+        const body = await response.json();
+        setApiResponse(body.error);
+      }
+    } catch (error) {
+      setApiResponse(getErrorMessage(error));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Game Page (server address: {serverAddress})</p>
-      </header>
+    <div>
+      {apiResponse === 'Success' ? (
+        <div>Success</div>
+      ) : (
+        <>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={playerName}
+              onChange={event => setPlayerName(event.target.value)}
+            />
+            <button type="submit">Submit</button>
+          </form>
+          {apiResponse}
+        </>
+      )}
     </div>
   );
 }
