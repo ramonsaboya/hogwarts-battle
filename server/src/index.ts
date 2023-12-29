2f57b67a124c80c2fb450f4c1bde42166fd7cbd2
Ramon Saboya <ramonsaboya@fb.com>
2023-12-29 19:38:38 +0200

    Basic server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,26 +1,9 @@
-console.log("Try npm run lint/fix!");
+import express from 'express';
 
-const longString = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+const app = express();
+const port = 4000;
 
-const trailing = 'Semicolon'
-
-			const why={am:'I tabbed?'};
-
-const iWish = "I didn't have a trailing space..."; 
-
-const sicilian = true;;
-
-const vizzini = (!!sicilian) ? !!!sicilian : sicilian;
-
-const re = /foo   bar/;
-
-export function doSomeStuff(withThis: string, andThat: string, andThose: string[]) {
-    //function on one line
-    if(!Boolean(andThose.length)) {return false;}
-    console.log(withThis);
-    console.log(andThat);
-    console.dir(andThose);
-    console.log(longString, trailing, why, iWish, vizzini, re);
-    return;
-}
-// TODO: more examples
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
