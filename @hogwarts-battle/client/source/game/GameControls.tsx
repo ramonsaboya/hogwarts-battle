f3868b927bee7fef8f05fbca4cfa1cbfe6cb4db8
Ramon Saboya <ramonsaboya@fb.com>
2024-01-06 21:16:22 +0200

    Disable endTurn button if not own turn

--- @hogwarts-battle/client/source/game/GameControls.tsx
+++ @hogwarts-battle/client/source/game/GameControls.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {useAction} from '../socket/useAction';
 import GameContextDisplay from './GameContextDisplay';
 import {createUseStyles} from 'react-jss';
+import {usePlayerView} from './PlayerViewContext';
 
 const useStyles = createUseStyles({
   container: {
@@ -13,16 +14,25 @@ const useStyles = createUseStyles({
 
 export default function GameControls() {
   const classes = useStyles();
+  const {gameContext, gameStateView} = usePlayerView();
+
+  const isOwnTurn =
+    gameContext.currentPlayer === gameStateView.players.selfPlayer.playerID;
 
   const runAction = useAction();
   const handleEndTurn = () => {
+    if (!isOwnTurn) {
+      return;
+    }
     runAction({action: 'endTurn', args: {}});
   };
 
   return (
     <div className={classes.container}>
       <GameContextDisplay />
-      <button onClick={handleEndTurn}>End Turn</button>
+      <button onClick={handleEndTurn} disabled={!isOwnTurn}>
+        End Turn
+      </button>
     </div>
   );
 }
